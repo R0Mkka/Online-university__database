a032f1b079f743cfe522da01763a0350c849f3b5
Alexanov Roman <alexanov.roman@gmail.com>
2020-04-26 02:27:25 +0300

    Timetable instances add

--- ou_new/draft.sql
+++ ou_new/draft.sql
@@ -3,10 +3,14 @@ USE online_university;
 describe chats;
 describe courses;
 
+select * from users;
+select * from users_statuses;
 select * from courses;
 select * from courses_data;
-select * from users_courses;
+select * from users_chats;
 select * from chats;
+select * from days_of_the_week;
+select * from timetable_items;
 
 ALTER TABLE courses MODIFY courseCode VARCHAR(20) NOT NULL UNIQUE;
 
@@ -19,5 +23,26 @@ DROP TABLE courses;
 DROP TABLE courses_data;
 
 DELETE
-FROM chats
-WHERE chatId > 0;
+FROM users_chats
+WHERE userId <> 2;
+
+select
+	*
+from
+	users_entries left join (
+		select MAX(userEntryId) userEntryId
+        from users_entries
+        group by userId
+	) maxes
+using(userEntryId)
+group by userId;
+
+select 
+	userId,
+    userStatusId,
+    enteredAt
+from users_entries
+where (SELECT MAX(userEntryId) from users_entries group by userId)
+group by userId;
+
+select * from users_entries group by userId order by enteredAt DESC;
---
+++ ou_new/timetable.sql
@@ -0,0 +1,102 @@
+USE online_university;
+
+CREATE TABLE IF NOT EXISTS days_of_the_week (
+	dayOfTheWeekId TINYINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(15)
+);
+
+CREATE TABLE IF NOT EXISTS timetable_item_groups (
+	timetableItemGroupId INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    userId INT UNSIGNED NOT NULL,
+    name NVARCHAR(30) NOT NULL,
+    
+    PRIMARY KEY (timetableItemGroupId),
+    
+    CONSTRAINT fk_TIG_userId
+    FOREIGN KEY (userId)
+    REFERENCES users(userId)
+		ON UPDATE CASCADE
+        ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS timetable_item_stickers (
+	timetableItemStickerId INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    userId INT UNSIGNED NOT NULL,
+    title NVARCHAR(30) NOT NULL,
+    color VARCHAR(20) NOT NULL,
+    
+    PRIMARY KEY (timetableItemStickerId),
+    
+    CONSTRAINT fk_TIS_userId
+    FOREIGN KEY (userId)
+    REFERENCES users(userId)
+		ON UPDATE CASCADE
+        ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS timetable_items (
+	timetableItemId INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    userId INT UNSIGNED NOT NULL,
+    dayOfTheWeekId TINYINT UNSIGNED NOT NULL,
+    courseId INT UNSIGNED,
+    timetableItemGroupId INT UNSIGNED,
+    subject NVARCHAR(100) NOT NULL,
+    teacherFullName NVARCHAR(50) NOT NULL,
+    onlineMeeting TEXT,
+    classroom NVARCHAR(15),
+    startTime TIME NOT NULL,
+    endTime TIME NOT NULL,
+    
+    PRIMARY KEY (timetableItemId),
+    
+	CONSTRAINT fk_TI_userId
+    FOREIGN KEY (userId)
+    REFERENCES users(userId)
+		ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    
+    CONSTRAINT fk_TI_dayOfTheWeekId
+    FOREIGN KEY (dayOfTheWeekId)
+    REFERENCES days_of_the_week(dayOfTheWeekId)
+		ON UPDATE CASCADE,
+        
+	CONSTRAINT fk_TI_courseId
+    FOREIGN KEY (courseId)
+    REFERENCES courses(courseId)
+		ON UPDATE CASCADE,
+        
+	CONSTRAINT fk_TI_timetableItemGroupId
+    FOREIGN KEY (timetableItemGroupId)
+    REFERENCES timetable_item_groups(timetableItemGroupId)
+		ON UPDATE CASCADE
+        ON DELETE CASCADE
+);
+
+ALTER TABLE timetable_items
+ADD COLUMN teacherFullName NVARCHAR(50) NOT NULL;
+
+CREATE TABLE IF NOT EXISTS timetable_items_timetable_items_stickers (
+	timetableItemId INT UNSIGNED NOT NULL,
+    timetableItemStickerId INT UNSIGNED NOT NULL,
+    
+    CONSTRAINT fk_TITIS_timetableItemId
+    FOREIGN KEY (timetableItemId)
+    REFERENCES timetable_items(timetableItemId)
+		ON UPDATE CASCADE
+        ON DELETE CASCADE,
+        
+	CONSTRAINT fk_TITIS_timetableItemStickerId
+    FOREIGN KEY (timetableItemStickerId)
+    REFERENCES timetable_item_stickers(timetableItemStickerId)
+		ON UPDATE CASCADE
+        ON DELETE CASCADE
+);
+
+INSERT INTO days_of_the_week (name)
+VALUES ('Monday'),
+	   ('Tuesday'),
+       ('Wednesday'),
+       ('Thursday'),
+       ('Friday'),
+       ('Saturday'),
+       ('Sunday');
